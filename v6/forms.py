9059a925bac3bc2d1e943e6e72a66b4e38f4fd85
Edvinas Afarjancas <eafarjancas@gmail.com>
2023-06-20 22:11:57 +0300

    updated with password change

---
+++ v6/forms.py
@@ -0,0 +1,42 @@
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, BooleanField, StringField, PasswordField
+from wtforms.validators import DataRequired, ValidationError, EqualTo, Length
+import app
+
+
+class RegistracijosForma(FlaskForm):
+    vardas = StringField('Vardas', [DataRequired()])
+    el_pastas = StringField('El. paštas', [DataRequired()])
+    slaptazodis = PasswordField('Slaptažodis', [DataRequired()])
+    patvirtintas_slaptazodis = PasswordField("Pakartokite slaptažodį", [
+                                             EqualTo('slaptazodis', "Slaptažodis turi sutapti.")])
+    submit = SubmitField('Prisiregistruoti')
+
+    def tikrinti_varda(self, vardas):
+        vartotojas = app.Vartotojas.query.filter_by(vardas=vardas.data).first()
+        if vartotojas:
+            raise ValidationError('Šis vardas panaudotas. Pasirinkite kitą.')
+
+    def tikrinti_pasta(self, el_pastas):
+        vartotojas = app.Vartotojas.query.filter_by(
+            el_pastas=el_pastas.data).first()
+        if vartotojas:
+            raise ValidationError(
+                'Šis el. pašto adresas panaudotas. Pasirinkite kitą.')
+
+
+class PrisijungimoForma(FlaskForm):
+    el_pastas = StringField('El. paštas', [DataRequired()])
+    slaptazodis = PasswordField('Slaptažodis', [DataRequired()])
+    prisiminti = BooleanField("Prisiminti mane")
+    submit = SubmitField('Prisijungti')
+
+
+class PasswordChangeForma(FlaskForm):
+    senas_slaptazodis = PasswordField(
+        "Senas slaptažodis", [DataRequired()])
+    naujas_slaptazodis = PasswordField(
+        "Naujas slaptažodis", [DataRequired(), Length(min=8)])
+    patvirtintas_slaptazodis = PasswordField(
+        "Pakartokite naują slaptažodį", [EqualTo("naujas_slaptazodis")])
+    submit = SubmitField("Keisti slaptažodį")
